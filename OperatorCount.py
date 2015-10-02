9af113fa4904adceffa0afb4f150656440137ba3
Simon O <saouderkirk@gmail.com>
2015-10-02 13:55:50 -0400

    Getting Started
    Output works, have to figure out how to parse by date/time

--- OperatorCount.py
+++ OperatorCount.py
@@ -1,1 +1,25 @@
-OperatorCount.py
+import csv
+
+remove_from = 4
+remove_to = 10000
+
+with open('olark_report_20150922_20150929.csv', 'rb') as fp_in, open("output.csv", "wb") as fp_out:
+    reader = csv.reader(fp_in, delimiter=",")
+    writer = csv.writer(fp_out, delimiter=",")
+    for row in reader:
+        del row[remove_from:remove_to]
+        writer.writerow(row)
+
+with open('output.csv')
+
+
+# Pull in CSV from Olark w/ one week's information & create new CSV for output
+
+# Drop all of the unused columns
+
+# Break timestamps into days and hours
+
+# Count number of operators with at least one chat in each hours
+
+# Append hourly operator count to output CSV
+
