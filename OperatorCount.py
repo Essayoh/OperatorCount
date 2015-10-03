3fcba0df128bfbad609de86f5816bd25ae87c9e6
Simon O <saouderkirk@gmail.com>
2015-10-03 10:14:28 -0400

    Data Cleaned
    Go from big .csv output from Olark to two-column timestamp and
    operator.

--- OperatorCount.py
+++ OperatorCount.py
@@ -10,7 +10,13 @@ with open('olark_report_20150922_20150929.csv', 'rb') as fp_in, open("output.csv
         del row[remove_from:remove_to]
         writer.writerow(row)
 
-with open('output.csv')
+with open('output.csv') as week_in, open('output2.csv', "wb") as week_out:
+	reader = csv.reader(week_in, delimiter=",")
+	writer = csv.writer(week_out, delimiter=",")
+	for row in reader:
+		del row[1:3]
+		writer.writerow(row)
+
 
 
 # Pull in CSV from Olark w/ one week's information & create new CSV for output
