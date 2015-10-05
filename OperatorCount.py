ec6c6860ba6649d0b1ce27c8c53b5bd1c38bc731
Simon O <saouderkirk@gmail.com>
2015-10-05 06:53:24 -0400

    All data formatted
    Now to output the count

--- OperatorCount.py
+++ OperatorCount.py
@@ -1,31 +1,52 @@
 import csv
+import os
 
-remove_from = 4
-remove_to = 10000
+timestamps = []
+rawdates = []
+dates = []
+rawtimes = []
+times = []
+opers = []
+timelist = ['00','01','02','03','04','05','06','07','08','09','10','11','12','13','14','15','16','17','18','19','20','21','22','23','24']
 
-with open('olark_report_20150922_20150929.csv', 'rb') as fp_in, open("output.csv", "wb") as fp_out:
-    reader = csv.reader(fp_in, delimiter=",")
-    writer = csv.writer(fp_out, delimiter=",")
+#import Olark file, pull timestamps and operators
+
+with open('olark_report_20150922_20150929.csv', 'rb') as olark_in:
+    reader = csv.reader(olark_in, delimiter=',')
     for row in reader:
-        del row[remove_from:remove_to]
-        writer.writerow(row)
+        timestamps.append(row[0])
+        opers.append(row[3])
 
-with open('output.csv') as week_in, open('output2.csv', "wb") as week_out:
-	reader = csv.reader(week_in, delimiter=",")
-	writer = csv.writer(week_out, delimiter=",")
-	for row in reader:
-		del row[1:3]
-		writer.writerow(row)
+#splitting time and date
 
+for i in timestamps:
+	rawdates.append(i.split(' ',1)[0])
+	rawtimes.append(i.split(' ',1)[-1])
 
+#deleting headers
 
-# Pull in CSV from Olark w/ one week's information & create new CSV for output
+del timestamps[0], rawdates[0], rawtimes[0], opers[0]
 
-# Drop all of the unused columns
+#formatting dates
 
-# Break timestamps into days and hours
+for i in rawdates:
+	if i not in dates:
+		dates.append(i)
+
+dates.reverse()
+
+#formatting times
+
+for i in rawtimes:
+	times.append(i.split(':',1)[0])
+
+#exporting to new .csv
+
+with open('output.csv', 'wb') as olark_out:
+	writer = csv.writer(olark_out, delimiter = ',')
+	for date in dates:
+			writer.writerow([date])
+
 
 # Count number of operators with at least one chat in each hours
-
-# Append hourly operator count to output CSV
 
