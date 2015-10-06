a97e6b028fda4921e757048a3d53accfb86a1f64
Simon O <saouderkirk@gmail.com>
2015-10-06 06:46:55 -0400

    Really getting into it
    Data fully cleaned, trying to sort out multi level array for date /
    hour:count

--- OperatorCount.py
+++ OperatorCount.py
@@ -7,7 +7,11 @@ dates = []
 rawtimes = []
 times = []
 opers = []
-timelist = ['00','01','02','03','04','05','06','07','08','09','10','11','12','13','14','15','16','17','18','19','20','21','22','23','24']
+timelist = ['00','01','02','03','04','05','06','07',
+			'08','09','10','11','12','13','14','15',
+			'16','17','18','19','20','21','22','23']
+csvoutput = []
+
 
 #import Olark file, pull timestamps and operators
 
@@ -16,6 +20,14 @@ with open('olark_report_20150922_20150929.csv', 'rb') as olark_in:
     for row in reader:
         timestamps.append(row[0])
         opers.append(row[3])
+
+#create output file and headers
+
+with open('output.csv', 'wb') as output:
+	writer = csv.writer(output, delimiter=',')
+	writer.writerow(['Date','00','01','02','03','04','05','06','07'
+					  ,'08','09','10','11','12','13','14','15','16',
+					   '17','18','19','20','21','22','23'])
 
 #splitting time and date
 
@@ -27,26 +39,50 @@ for i in timestamps:
 
 del timestamps[0], rawdates[0], rawtimes[0], opers[0]
 
-#formatting dates
+#formatting dates & times
 
 for i in rawdates:
 	if i not in dates:
 		dates.append(i)
 
-dates.reverse()
+print dates
 
-#formatting times
+dates.reverse()
+csvoutput.append(dates)
 
 for i in rawtimes:
-	times.append(i.split(':',1)[0])
+	times.append(int(i.split(':',1)[0]))
+
+#formatting data - need 7 columns of 24 values each
+#so need to check and count for each hour and output operator count
+#maybe an array - date: hour:count, so each date will have 24 hour:count key:value pairs
+
+
+#using rawdates here b/c dates has only 7 entries and won't map onto full data, only first 7 entries
+workdata = zip(rawdates,times,opers)
+
+count = 00
+array = [
+
+
+]
+
+for i in workdata:
+	if i[1] == count:
+		opcount.append(i[2])
+
+len(set(opcount))
+
+
+		
+
+
+
 
 #exporting to new .csv
 
-with open('output.csv', 'wb') as olark_out:
-	writer = csv.writer(olark_out, delimiter = ',')
-	for date in dates:
-			writer.writerow([date])
-
-
-# Count number of operators with at least one chat in each hours
+#with open('output.csv', 'wb') as olark_out:
+#	writer = csv.writer(olark_out, delimiter = ',')
+#	for date in dates:
+#			writer.writerow([date])
 
